189170c230d3b179b2ca99b3d4453bda8a20ff12
lauer-safenow <lauer@safenow.de>
2025-02-22 23:05:07 +0100

    feat: improve vacation absence handling with validation and overlap checks

--- safenow-gone/src/main/kotlin/de/safenow/domain/Employee.kt
+++ safenow-gone/src/main/kotlin/de/safenow/domain/Employee.kt
@@ -1,5 +1,6 @@
 package de.safenow.domain
 
+import com.aayushatharva.brotli4j.common.annotations.Local
 import java.time.LocalDate
 
 data class Employee(
@@ -28,22 +29,38 @@ fun Employee.removeAbsence(vacation: Vacation): Pair<Boolean, Employee> {
     return false to this
 }
 
-fun Employee.addAbsence(v: Vacation): Pair<Boolean, Employee> {
-    // TODO check for time overlaps not only relying on SET
-    val from = v.from
-    val to = v.to
-    val isOverlapping =
-        this.absences.any { (it.from <= from && it.to >= from) || (it.from <= to && it.to >= to) || (it.from == from && it.to == to) }
-    if (!isOverlapping) {
-        val newAbsences = this.absences.toMutableSet()
-        newAbsences.add(Absence(from, to, "Vacation"))
-        val newDaysTaken = v.from.until(v.to).days + 1 + this.daysTaken
-        return true to this.copy(daysTaken = newDaysTaken, absences = newAbsences)
-    } else {
-        return false to this
+fun Employee.addAbsence(v: Vacation): Employee {
+    validateVacationParams(v)
+    validateOverlapping(v)
+
+    val newAbsences = this.absences.toMutableSet()
+    newAbsences.add(Absence(v.from, v.to, "Vacation"))
+    val newDaysTaken = 1 + v.from.until(v.to).days + this.daysTaken
+    return this.copy(daysTaken = newDaysTaken, absences = newAbsences)
+
+
+}
+
+private fun Employee.validateVacationParams(v: Vacation) {
+    if (v.from.isAfter(v.to)) {
+        throw SomeException(
+            """Vacation <<FROM>> may not be after <<TO>>
+                    the past employee:${this.id} from:${v.from} to:${v.to}"""
+        )
     }
+}
 
+private fun Employee.validateOverlapping(v: Vacation) {
+    if (isOverlapping(v.from, v.to)) {
+        throw SomeException("Vacation overlaps with existing absence employee:${this.id} from: ${v.from} to: ${v.to}")
+    }
+}
 
+private fun Employee.isOverlapping(from: LocalDate, to: LocalDate): Boolean {
+    val isOverlapping = this.absences.any {
+        (it.from <= from && it.to >= from) || (it.from <= to && it.to >= to) || (it.from == from && it.to == to)
+    }
+    return isOverlapping
 }
 
 
